1b5b212a42f43ef3288daadc5b80cfc5845c0044
green <grzx2210@163.com>
2024-07-12 11:47:21 +0800

    Create init_env_windows.bat

---
+++ init_env_windows.bat
@@ -0,0 +1,16 @@
+@echo off
+# 双击运行该批处理文件，或在命令提示符中导航到文件所在目录并运行 init_env_windows.bat
+
+:: Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt -U
+
+:: Set environment variable for FFMPEG_PATH
+set "FFMPEG_PATH=%cd%\ffmpeg-4.4-amd64-static"
+set PATH=%FFMPEG_PATH%;%PATH%
+
+echo "FFMPEG_PATH is set to %FFMPEG_PATH%"
+echo "Environment setup complete. You can now run your application."
+
+:: Keep the window open
+pause
